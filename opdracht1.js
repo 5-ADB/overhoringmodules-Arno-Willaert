65917ff9ad4c205a3e9bf12bfa94e10a395f18eb
Arno-Willaert <Arno.willaert@student.tsaam.be>
2025-06-02 11:21:26 +0200

    commit1

--- opdracht1.js
+++ opdracht1.js
@@ -1,2 +1,20 @@
 // Maak een objectmodel aan voor een leerling
 // zorg ervoor dat de leerling in een klas kan zitten. 
+
+leerling = {
+    naam:"Els",
+    achternaam : "Dotjes",
+    leeftijd : 16,
+    Inklas : false
+}
+
+function Klas(student){
+    if (student.Inklas == true) {
+        console.log(`${student.naam} ${student.achternaam} zit in de klas.`);
+    }
+    else(student.Inklas == false);{
+        console.log(`${student.naam} ${student.achternaam} zit niet in de klas.`);
+    }
+}
+
+Klas(leerling)
--- opdracht2.js
+++ opdracht2.js
@@ -4,7 +4,33 @@ const factuur = {
     id: 1,
     klant: "Jos Joskens",
     producten: [
-        appel, peer, banaan
+        "appel", "peer", "banaan"
     ],
     betaald: false
 }
+
+// optie 1
+function Factuur1(id, klant, producten, betaald){
+    this.id = id,
+    this.klant = klant,
+    this.producten = producten
+    this.betaald = betaald
+}
+
+const rekening1 = new Factuur1(1, "pieter jansen", "appels, bananen, citroenen", "nee")
+console.log(rekening1);
+
+
+// optie 2
+
+function Factuur2(id, klant, producten, betaald){
+    return{
+        id,
+        klant,
+        producten,
+        betaald,
+    }
+}
+
+const rekening2 = new Factuur2(2, "Els Dotjes", "Worsten", "Ja")
+console.log(rekening2);
